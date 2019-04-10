2ce6a1518a16e9ff43738058b764d80af1c44735
Tony Baker <this.tonybaker@gmail.com>
2019-04-10 16:29:54 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,11 +1,12 @@
 class Owner
   attr_accessor
   attr_writer
-  attr_reader :name
+  attr_reader :name, :species
 
   @@all = []
   def initialize(name)
     @name = name
+    @species = "human"
     @@all << self
   end
 
