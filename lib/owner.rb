7c0622ddc697c40993fe312f200eda7a289f3000
Tony Baker <this.tonybaker@gmail.com>
2019-04-10 17:24:59 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -59,4 +59,8 @@ class Owner
     self.pets = {:fishes => [], :dogs => [], :cats => []}
   end
 
+  def list_pets
+    "I have #{self.pets[:fishes].size} fish, #{self.pets[:dogs].size} dog(s), and #{self.pets[:cats].size} cat(s)."
+  end
+
 end
